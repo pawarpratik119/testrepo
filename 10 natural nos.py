a0419550988de052e10a39bd6361615a0fe90c3c
pawarpratik119 <83055274+pawarpratik119@users.noreply.github.com>
2021-04-23 11:01:45 +0530

    Add files via upload
    Addition of 10 consecutive natural nos

---
+++ 10 natural nos.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 19:04:04 2020
+
+@author: Admin
+"""
+
+#Example for sum of 10 natural nos
+sum = 0
+i = 1
+n = int(input("enter a natural number:  "))
+while i<=n:
+    sum=sum+i
+    i=i+1
+else:
+    print("sum of",n,"natural number=",sum)
+    
